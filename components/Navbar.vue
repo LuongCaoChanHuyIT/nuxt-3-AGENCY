ae1f72b2ecadc7a683a7d1c3661855da7695f86c
Lương Cao Chấn Huy <161787746+LuongCaoChanHuyIT@users.noreply.github.com>
2025-08-30 08:28:54 +0700

    first commit

---
+++ components/Navbar.vue
@@ -0,0 +1,75 @@
+<template lang="">
+  <div :data-bs-theme="theme.isDark ? 'dark' : 'light'">
+    <nav
+      class="navbar navbar-expand-lg py-3"
+      :class="theme.isDark ? 'navbar-dark bg-black' : 'navbar-light bg-white'"
+    >
+      <div class="container px-5">
+        <a class="navbar-brand" href="index.html"
+          ><span class="fw-bolder text-primary">{{
+            $t("navbar.namePorfolio")
+          }}</span></a
+        >
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav ms-auto mb-2 mb-lg-0 small fw-bolder">
+            <li class="nav-item">
+              <NuxtLink to="/" class="nav-link">{{
+                $t("navbar.home")
+              }}</NuxtLink>
+            </li>
+            <li class="nav-item">
+              <NuxtLink to="/resume" class="nav-link">{{
+                $t("navbar.about")
+              }}</NuxtLink>
+            </li>
+            <li class="nav-item">
+              <NuxtLink to="/projects" class="nav-link">{{
+                $t("navbar.projects")
+              }}</NuxtLink>
+            </li>
+            <li class="nav-item">
+              <NuxtLink to="/contact" class="nav-link">{{
+                $t("navbar.contact")
+              }}</NuxtLink>
+            </li>
+          </ul>
+          <button
+            class="btn btn-outline-primary btn-sm ms-3"
+            @click="toggleLanguage"
+          >
+            {{ locale === "en" ? "VI" : "EN" }}
+          </button>
+          <button
+            class="btn btn-outline-primary btn-sm ms-3"
+            @click="theme.toggleDark()"
+          >
+            {{ theme.isDark ? $t("theme.light") : $t("theme.dark") }}
+          </button>
+        </div>
+      </div>
+    </nav>
+  </div>
+</template>
+<script setup lang="ts">
+import { useThemeStore } from "@/stores/theme";
+const theme = useThemeStore();
+const { locale, setLocale } = useI18n();
+
+const toggleLanguage = async () => {
+  const newLocale = locale.value === "en" ? "vi" : "en";
+  await setLocale(newLocale);
+};
+</script>
+
+<style lang=""></style>
---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <Header />
+    <About />
+  </div>
+</template>
+
+<script setup>
+// const { $supabase } = useNuxtApp();
+// debugger;
+// const { data, error } = await $supabase
+//   .from("homepage_sections") //bảng này ad đã định nghĩa ở superbase rồi
+//   .select("*") //select 1 số cột nếu muốn.
+//   .order("created_at", { ascending: false })
+//   .limit(10); //10 or số nào cũng đc
+
+// console.log(data);
+
+// if (error) {
+//   console.error(error.message);
+// }
+</script>
