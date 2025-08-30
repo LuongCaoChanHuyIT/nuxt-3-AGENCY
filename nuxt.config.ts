b5fa29dd14c4af4677b0d9bebd3436a7cafb9a40
Lương Cao Chấn Huy <161787746+LuongCaoChanHuyIT@users.noreply.github.com>
2025-08-30 22:05:17 +0700

    save code

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,7 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   css: [
     "animate.css",
-    // "bootstrap/dist/css/bootstrap.min.css",
+    "bootstrap/dist/css/bootstrap.min.css",
     // "bootstrap-icons/font/bootstrap-icons.css",
     "~/public/css/style.css",
     "~/public/css/plugins.css",
@@ -13,73 +13,23 @@ export default defineNuxtConfig({
   ],
   plugins: [{ src: "~/plugins/bootstrap.client.ts", mode: "client" }],
   modules: ["@nuxtjs/i18n", "@pinia/nuxt", "@pinia-plugin-persistedstate/nuxt"],
-  // @ts-ignore
-  i18n: {
-    locales: [
-      {
-        code: "en",
-        name: "English",
-        iso: "en-US",
-        file: "en.json",
-      },
-      {
-        code: "vi",
-        name: "Tiếng Việt",
-        iso: "vi-VN",
-        file: "vi.json",
-      },
-    ],
-    langDir: "locales/",
-    defaultLocale: "en",
-    strategy: "prefix_except_default",
-    // @ts-ignore
-    lazy: true,
-    detectBrowserLanguage: {
-      useCookie: true,
-      cookieKey: "i18n_redirected",
-      redirectOn: "root",
-    },
-  },
   app: {
     head: {
       link: [
-        // {
-        //   rel: "stylesheet",
-        //   href: "https://fonts.googleapis.com/css2?family=Urbanist:wght@400;500;700&family=Roboto:wght@400;700&display=swap",
-        // },
         {
           rel: "stylesheet",
-          href: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css",
-        },
-        {
-          rel: "stylesheet",
-          href: "https://unicons.iconscout.com/release/v4.0.8/css/line.css",
-        },
-        {
-          rel: "preload",
-          href: "css/urbanist.css",
-          as: "style",
-          onload: "this.rel='stylesheet'",
+          href: "https://fonts.googleapis.com/css2?family=Urbanist:wght@300;400;500;600;700&display=swap",
         },
       ],
       script: [
         {
-          src: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js",
-          tagPosition: "bodyClose",
-        },
-        { src: "/js/theme.js", tagPosition: "bodyClose" },
-        {
-          src: "/js/email.js",
+          src: "/js/theme.js",
           tagPosition: "bodyClose",
         },
         {
           src: "/js/plugins.js",
           tagPosition: "bodyClose",
         },
-        // {
-        //   src: "/js/theme.js",
-        //   tagPosition: "bodyClose",
-        // },
       ],
     },
   },
