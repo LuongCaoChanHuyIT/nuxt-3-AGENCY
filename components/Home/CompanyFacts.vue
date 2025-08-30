57333330e992eda1627fe47b3e2b56e3d2176fa3
Lương Cao Chấn Huy <161787746+LuongCaoChanHuyIT@users.noreply.github.com>
2025-08-30 15:40:02 +0700

    component: component home divide done

---
+++ components/Home/CompanyFacts.vue
@@ -0,0 +1,367 @@
+<template lang="">
+  <div>
+    <section class="wrapper bg-light">
+      <div class="container pt-12 pt-lg-8 pb-14 pb-md-16">
+        <div class="row gx-lg-8 gx-xl-12 gy-10 gy-lg-0 mb-11">
+          <div class="col-lg-4 text-center text-lg-start">
+            <h2 class="fs-16 text-uppercase text-primary mb-3">
+              Company Facts
+            </h2>
+            <h3 class="display-3 mb-4 pe-xxl-5">
+              We are proud of our design team
+            </h3>
+            <p class="mb-0 pe-xxl-11">
+              Just sit back and relax while we take care of your business needs
+              for you.
+            </p>
+          </div>
+          <!-- /column -->
+          <div class="col-lg-8 mt-lg-2">
+            <div
+              class="row align-items-center counter-wrapper gy-6 text-center"
+            >
+              <div class="col-md-4">
+                <div
+                  class="icon btn btn-circle btn-lg btn-soft-primary pe-none mb-4"
+                >
+                  <i class="uil uil-presentation-check"></i>
+                </div>
+                <h3 class="counter">1000+</h3>
+                <p>Completed Projects</p>
+              </div>
+              <!--/column -->
+              <div class="col-md-4">
+                <div
+                  class="icon btn btn-circle btn-lg btn-soft-primary pe-none mb-4"
+                >
+                  <i class="uil uil-user-check"></i>
+                </div>
+                <h3 class="counter">50K+</h3>
+                <p>Happy Customers</p>
+              </div>
+              <!--/column -->
+              <div class="col-md-4">
+                <div
+                  class="icon btn btn-circle btn-lg btn-soft-primary pe-none mb-4"
+                >
+                  <i class="uil uil-trophy"></i>
+                </div>
+                <h3 class="counter">20+</h3>
+                <p>Awards Won</p>
+              </div>
+              <!--/column -->
+            </div>
+            <!--/.row -->
+          </div>
+          <!-- /column -->
+        </div>
+        <!-- /.row -->
+        <div class="row grid-view gy-6 gy-xl-0">
+          <div class="col-md-6 col-xl-3">
+            <div class="card shadow-lg">
+              <div class="card-body">
+                <img
+                  class="rounded-circle w-15 mb-4"
+                  src="/images/te1.jpg"
+                  srcset="/images/te1@2x.jpg 2x"
+                  alt=""
+                />
+                <h4 class="mb-1">Coriss Ambady</h4>
+                <div class="meta mb-2">Financial Analyst</div>
+                <p class="mb-2">
+                  Fermentum massa justo sit amet risus morbi leo.
+                </p>
+                <nav class="nav social mb-0">
+                  <a href="#"><i class="uil uil-twitter"></i></a>
+                  <a href="#"><i class="uil uil-facebook-f"></i></a>
+                  <a href="#"><i class="uil uil-dribbble"></i></a>
+                </nav>
+                <!-- /.social -->
+              </div>
+              <!--/.card-body -->
+            </div>
+            <!-- /.card -->
+          </div>
+          <!--/column -->
+          <div class="col-md-6 col-xl-3">
+            <div class="card shadow-lg">
+              <div class="card-body">
+                <img
+                  class="rounded-circle w-15 mb-4"
+                  src="/images/te2.jpg"
+                  srcset="/images/te2@2x.jpg 2x"
+                  alt=""
+                />
+                <h4 class="mb-1">Cory Zamora</h4>
+                <div class="meta mb-2">Marketing Specialist</div>
+                <p class="mb-2">
+                  Fermentum massa justo sit amet risus morbi leo.
+                </p>
+                <nav class="nav social mb-0">
+                  <a href="#"><i class="uil uil-twitter"></i></a>
+                  <a href="#"><i class="uil uil-facebook-f"></i></a>
+                  <a href="#"><i class="uil uil-dribbble"></i></a>
+                </nav>
+                <!-- /.social -->
+              </div>
+              <!--/.card-body -->
+            </div>
+            <!-- /.card -->
+          </div>
+          <!--/column -->
+          <div class="col-md-6 col-xl-3">
+            <div class="card shadow-lg">
+              <div class="card-body">
+                <img
+                  class="rounded-circle w-15 mb-4"
+                  src="/images/te3.jpg"
+                  srcset="/images/te3@2x.jpg 2x"
+                  alt=""
+                />
+                <h4 class="mb-1">Nikolas Brooten</h4>
+                <div class="meta mb-2">Sales Manager</div>
+                <p class="mb-2">
+                  Fermentum massa justo sit amet risus morbi leo.
+                </p>
+                <nav class="nav social mb-0">
+                  <a href="#"><i class="uil uil-twitter"></i></a>
+                  <a href="#"><i class="uil uil-facebook-f"></i></a>
+                  <a href="#"><i class="uil uil-dribbble"></i></a>
+                </nav>
+                <!-- /.social -->
+              </div>
+              <!--/.card-body -->
+            </div>
+            <!-- /.card -->
+          </div>
+          <!--/column -->
+          <div class="col-md-6 col-xl-3">
+            <div class="card shadow-lg">
+              <div class="card-body">
+                <img
+                  class="rounded-circle w-15 mb-4"
+                  src="/images/te4.jpg"
+                  srcset="/images/te4@2x.jpg 2x"
+                  alt=""
+                />
+                <h4 class="mb-1">Jackie Sanders</h4>
+                <div class="meta mb-2">Investment Planner</div>
+                <p class="mb-2">
+                  Fermentum massa justo sit amet risus morbi leo.
+                </p>
+                <nav class="nav social mb-0">
+                  <a href="#"><i class="uil uil-twitter"></i></a>
+                  <a href="#"><i class="uil uil-facebook-f"></i></a>
+                  <a href="#"><i class="uil uil-dribbble"></i></a>
+                </nav>
+                <!-- /.social -->
+              </div>
+              <!--/.card-body -->
+            </div>
+            <!-- /.card -->
+          </div>
+          <!--/column -->
+        </div>
+        <!--/.row -->
+      </div>
+      <!-- /.container -->
+      <div class="overflow-hidden">
+        <div class="container pt-12 pt-lg-7 pb-14 pb-md-16">
+          <div class="row">
+            <div class="col-lg-10 col-xl-9 col-xxl-8 mx-auto text-center">
+              <h2 class="fs-16 text-uppercase text-primary mb-3">
+                Latest Projects
+              </h2>
+              <h3 class="display-3 mb-10">
+                Check out some of our awesome projects with creative ideas and
+                great design.
+              </h3>
+            </div>
+            <!-- /column -->
+          </div>
+          <!-- /.row -->
+          <div
+            class="swiper-container grid-view nav-bottom nav-color mb-14"
+            data-margin="30"
+            data-dots="false"
+            data-nav="true"
+            data-items-md="2"
+            data-items-xs="1"
+          >
+            <div class="swiper overflow-visible">
+              <div class="swiper-wrapper">
+                <div class="swiper-slide">
+                  <figure class="rounded mb-7">
+                    <a href="./single-project.html"
+                      ><img
+                        src="/images/sp1.jpg"
+                        srcset="/images/sp1@2x.jpg 2x"
+                        alt=""
+                    /></a>
+                  </figure>
+                  <div
+                    class="project-details d-flex justify-content-center flex-column"
+                  >
+                    <div class="post-header">
+                      <h2 class="post-title h3">
+                        <a href="./single-project.html" class="link-dark"
+                          >Cras Fermentum Sem</a
+                        >
+                      </h2>
+                      <div class="post-category text-ash">Mobile Design</div>
+                    </div>
+                    <!-- /.post-header -->
+                  </div>
+                  <!-- /.project-details -->
+                </div>
+                <!--/.swiper-slide -->
+                <div class="swiper-slide">
+                  <figure class="rounded mb-7">
+                    <a href="./single-project.html"
+                      ><img
+                        src="/images/sp2.jpg"
+                        srcset="/images/sp2@2x.jpg 2x"
+                        alt=""
+                    /></a>
+                  </figure>
+                  <div
+                    class="project-details d-flex justify-content-center flex-column"
+                  >
+                    <div class="post-header">
+                      <h2 class="post-title h3">
+                        <a href="./single-project.html" class="link-dark"
+                          >Venenatis Euismod Vehicula</a
+                        >
+                      </h2>
+                      <div class="post-category text-ash">Web Design</div>
+                    </div>
+                    <!-- /.post-header -->
+                  </div>
+                  <!-- /.project-details -->
+                </div>
+                <!--/.swiper-slide -->
+                <div class="swiper-slide">
+                  <figure class="rounded mb-7">
+                    <a href="./single-project.html"
+                      ><img
+                        src="/images/sp3.jpg"
+                        srcset="/images/sp3@2x.jpg 2x"
+                        alt=""
+                    /></a>
+                  </figure>
+                  <div
+                    class="project-details d-flex justify-content-center flex-column"
+                  >
+                    <div class="post-header">
+                      <h2 class="post-title h3">
+                        <a href="./single-project.html" class="link-dark"
+                          >Tortor Tellus Cursus</a
+                        >
+                      </h2>
+                      <div class="post-category text-ash">Stationary</div>
+                    </div>
+                    <!-- /.post-header -->
+                  </div>
+                  <!-- /.project-details -->
+                </div>
+                <!--/.swiper-slide -->
+                <div class="swiper-slide">
+                  <figure class="rounded mb-7">
+                    <a href="./single-project.html"
+                      ><img
+                        src="/images/sp4.jpg"
+                        srcset="/images/sp4@2x.jpg 2x"
+                        alt=""
+                    /></a>
+                  </figure>
+                  <div
+                    class="project-details d-flex justify-content-center flex-column"
+                  >
+                    <div class="post-header">
+                      <h2 class="post-title h3">
+                        <a href="./single-project.html" class="link-dark"
+                          >Ridiculus Sem Parturient</a
+                        >
+                      </h2>
+                      <div class="post-category text-ash">Web Application</div>
+                    </div>
+                    <!-- /.post-header -->
+                  </div>
+                  <!-- /.project-details -->
+                </div>
+                <!--/.swiper-slide -->
+                <div class="swiper-slide">
+                  <figure class="rounded mb-7">
+                    <a href="./single-project.html"
+                      ><img
+                        src="/images/sp5.jpg"
+                        srcset="/images/sp5@2x.jpg 2x"
+                        alt=""
+                    /></a>
+                  </figure>
+                  <div
+                    class="project-details d-flex justify-content-center flex-column"
+                  >
+                    <div class="post-header">
+                      <h2 class="post-title h3">
+                        <a href="./single-project.html" class="link-dark"
+                          >Cursus Sollicitudin Adipiscing</a
+                        >
+                      </h2>
+                      <div class="post-category text-ash">Web Design</div>
+                    </div>
+                    <!-- /.post-header -->
+                  </div>
+                  <!-- /.project-details -->
+                </div>
+                <!--/.swiper-slide -->
+                <div class="swiper-slide">
+                  <figure class="rounded mb-7">
+                    <a href="./single-project.html"
+                      ><img
+                        src="/images/sp6.jpg"
+                        srcset="/images/sp6@2x.jpg 2x"
+                        alt=""
+                    /></a>
+                  </figure>
+                  <div
+                    class="project-details d-flex justify-content-center flex-column"
+                  >
+                    <div class="post-header">
+                      <h2 class="post-title h3">
+                        <a href="./single-project.html" class="link-dark"
+                          >Fringilla Quam Vulputate</a
+                        >
+                      </h2>
+                      <div class="post-category text-ash">Stationary</div>
+                    </div>
+                    <!-- /.post-header -->
+                  </div>
+                  <!-- /.project-details -->
+                </div>
+                <!--/.swiper-slide -->
+              </div>
+              <!--/.swiper-wrapper -->
+            </div>
+            <!-- /.swiper -->
+          </div>
+          <!-- /.swiper-container -->
+        </div>
+        <!-- /.container -->
+        <div class="divider text-soft-primary mx-n2">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 100">
+            <path
+              fill="currentColor"
+              d="M1260,1.65c-60-5.07-119.82,2.47-179.83,10.13s-120,11.48-180,9.57-120-7.66-180-6.42c-60,1.63-120,11.21-180,16a1129.52,1129.52,0,0,1-180,0c-60-4.78-120-14.36-180-19.14S60,7,30,7H0v93H1440V30.89C1380.07,23.2,1319.93,6.15,1260,1.65Z"
+            />
+          </svg>
+        </div>
+      </div>
+      <!-- /.overflow-hidden -->
+    </section>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style lang=""></style>
